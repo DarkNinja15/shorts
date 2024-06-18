d53d188fbc3fdf54eb6519606842d743423dc25c
Animesh Shukla <animeshshukla1518@gmail.com>
2024-06-19 01:19:29 +0530

    fix: added login api

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -2,19 +2,21 @@ use std::env;
 use std::sync::Arc;
 
 use axum::http::StatusCode;
+use axum::response::IntoResponse;
 use axum::{Extension, Json};
-use bcrypt::DEFAULT_COST;
+use bcrypt::{verify, DEFAULT_COST};
 use diesel::prelude::*;
 use jsonwebtoken::errors::Error;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
+use serde_json::{json, Value};
 use crate::schema::users::dsl::*;
+
 
 use crate::{db::DbPool, models::user::User};
 
 use jsonwebtoken::{encode, Header, EncodingKey};
 
-pub async fn signup(Json(user):Json<User>,Extension(pool): Extension<Arc<DbPool>>)->Result<impl axum::response::IntoResponse, StatusCode>{
+pub async fn signup(Json(user):Json<User>,Extension(pool): Extension<Arc<DbPool>>)->Result<impl IntoResponse, StatusCode>{
     let mut conn = pool.get().expect("Failed to get database connection from pool");
     
     // check whether the user already exists
@@ -78,7 +80,61 @@ pub async fn signup(Json(user):Json<User>,Extension(pool): Extension<Arc<DbPool>
     ))
 }
 
-pub async fn login(){}
+pub async fn login(Json(user): Json<Value>,Extension(pool):Extension<Arc<DbPool>>)->Result<impl IntoResponse, StatusCode>{
+
+    let mut conn = pool.get().expect("Failed to get database connection from pool");
+
+    let user_email = match user.get("email") {
+        Some(e) => e.as_str().ok_or(StatusCode::BAD_REQUEST)?,
+        None => return Ok((StatusCode::BAD_REQUEST, Json(json!({"error":"Email is required"})))),
+    };
+
+    let user_password = match user.get("password") {
+        Some(p)=>p.as_str().ok_or(StatusCode::BAD_REQUEST)?,
+        None=> return Ok((StatusCode::BAD_REQUEST, Json(json!({"error":"Password is required"})))
+        ),
+    };
+
+    tracing::debug!("email: {:?}", user_email);
+    tracing::debug!("password: {:?}", user_password);
+
+    let existing_user = match users.filter(email.eq(user_email))
+        .first::<User>(&mut conn)
+        .optional()
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR) {
+            Ok(Some(user)) => user,
+            Ok(None) => return Ok((StatusCode::UNAUTHORIZED, Json(json!({"error":"User not found"})))),
+            _ => return Ok((StatusCode::INTERNAL_SERVER_ERROR, Json(json!({"error":"Failed to get user"})))),
+        };
+    tracing::debug!("user_exist: {:?}", existing_user);
+
+    // decrypt the password
+    let is_password_valid=verify(user_password, existing_user.password.as_str()).map_err(|_|StatusCode::INTERNAL_SERVER_ERROR);
+
+    if let Err(_) = is_password_valid {
+        return Ok((StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"error":"Failed to verify password"}))
+        ));
+        
+    }
+
+    if is_password_valid.unwrap()==false {
+        return Ok((StatusCode::UNAUTHORIZED,
+            Json(json!({"error":"Invalid password"}))
+        ));
+    }
+
+    let token=genrate_token(existing_user.email.to_string());
+
+    if let Err(_) = token {
+        return Ok((StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"error":"Failed to generate token"}))
+        ));
+    }
+
+    Ok((StatusCode::OK,
+        Json(json!({"message":"User logged in successfully","user":existing_user,"token":token.unwrap()}))))
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
